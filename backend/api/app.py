fa7f144a47381d16f7258beaa01411ba71bd82ee
havraham@gmail.com <havraham@gmail.com>
2019-12-23 18:04:30 +0200

    login and signup

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,51 +1,80 @@
 from bson.objectid import ObjectId
 from flask import Flask, jsonify, request
-from flask_jwt_extended import create_access_token, JWTManager, jwt_required
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+
+from flask_bcrypt import Bcrypt, generate_password_hash,check_password_hash
+from flask_cors import CORS
+
+
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
-from werkzeug.security import safe_str_cmp
 
-
+import pprint
 import urllib.parse
 
 #from pprint import pprint
 
 #mongo = PyMongo(app)
-username = urllib.parse.quote_plus('username')
-#print(username)
-pwd = urllib.parse.quote_plus('password')
-#print(pwd)
-uri = 'mongodb://%s:%s@localhost:27017/salt' % (username,pwd)
+mongo_username = urllib.parse.quote_plus('username')
+# print(mongo_username)
+mongo_pwd = urllib.parse.quote_plus('password')
+# print(mongo_pwd)
+uri = 'mongodb://%s:%s@localhost:27017/salt' % (mongo_username,mongo_pwd)
 # print(uri)
 client = MongoClient(uri)
 db = client.salt
 users = db.users
-# print(db)
 
 app = Flask(__name__)
+CORS(app)
+bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
+
 
 # JWT Config
 app.config["JWT_SECRET_KEY"] = "this-is-secret-key" #change it
 
 
+@app.route("/register", methods=["POST"])
+def register():
+    if request.is_json:
+        first_name = request.json["first_name"]
+        last_name = request.json["last_name"]
+        email = request.json["email"]
+        password = request.json["password"]
+    else:
+        email = request.form["email"]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        password = request.form["password"]
 
+    # test = User.query.filter_by(email=email).first()
+    test = users.find_one({"email": email})
+    if test:
+        return jsonify(message="User Already Exist"), 409
+    
+    else:
+        user_info = dict(first_name=first_name, last_name=last_name, email=email, password=bcrypt.generate_password_hash(password))
+        print(user_info)
+        users.insert_one(user_info)
+        return jsonify(message="User added sucessfully"), 201
+        
 @app.route("/login", methods=["POST"])
 def login():
     if request.is_json:
-        user_name = request.json["username"]
-        pwd = request.json["password"]
+        email = request.json["email"]
+        password = request.json["password"]
     else:
-        user_name = request.form["username"]
-        pwd = request.form["password"]
+        email = request.form["email"]
+        password = request.form["password"]
 
-    print(user_name,pwd)
-    test = users.find_one({"username": user_name,"password":pwd})
+       
+    test = users.find_one({"email": email})
     if test:
-        access_token = create_access_token(identity=username)
-        return jsonify(message="Login Succeeded!", access_token=access_token), 201
-    else:
-        return jsonify(message="Bad username or Password"), 401
+        if check_password_hash(test["password"],password):
+            access_token = create_access_token(identity=email)
+            return jsonify(message="Login Succeeded!", access_token=access_token), 201
+    return jsonify(message="Bad Email or Password"), 401
 
 
 @app.route("/")
@@ -121,3 +150,7 @@ def get_events():
 		j['_id'] = str(j['_id'])
 		res.append(j)
 	return jsonify(res)
+
+
+if __name__ == '__main__':
+ app.run(debug=True)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './Dashboard';
 import * as serviceWorker from './serviceWorker';
+import LoginForm from './Component/LoginForm';
+import RegisterForm from './Component/RegisterForm';
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
 
