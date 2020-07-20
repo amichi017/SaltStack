d68602286ea3f0ecdbe63a99a11be576e9d44d04
amichi017 <amichi12345678@gmail.com>
2020-07-20 22:39:04 +0300

    add edit Team

--- frontend/src/reducers/CurrentMonth.js
+++ frontend/src/reducers/CurrentMonth.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import {CURRENT_MONTH} from '../actions/types';
 
@@ -22,29 +21,4 @@ switch(action.type){
    default:
        return state;
 }
-=======
-
-import {CURRENT_MONTH} from '../actions/types';
-
-// let temp=new Date();
-// let dataInit=[];
-// let mnonthDay =new Date(temp.getFullYear(), temp.getMonth()-1, 0).getDate();
-// for (let i=1;i<=mnonthDay;i++){
-//     dataInit.push( { name: String(i), Fail:0, Success:0 });
-// }
-const initialState = {
-    CurrentMonth:[],
-};
-
-export default function(state=initialState, action) { 
-   // console.log("CURRENT_MONTH",action.payload);
-switch(action.type){
-   case CURRENT_MONTH:
-   return {
-    CurrentMonth:action.payload,
-   };
-   default:
-       return state;
-}
->>>>>>> 00ce104ffbc429e346bb4ddc2db5d08d084aed1f
 }
